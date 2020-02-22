48e8ea831939547281fb8530f18b8d0722aadf9d
moodmedia_alinca <a.linca@moodmedia.com>
2020-02-22 23:19:26 +0200

    upgraded gulp upgraded es6-shim small cosmetic changes in gulpfile.js override default libs to add es2015 to prevent Promise errors when targetting es5

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,15 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
-var merge = require('merge2');
-
 var tsProject = ts.createProject('./src/tsconfig.json');
 
 gulp.task('scripts', function() {
-    var tsResult = gulp.src('src/*.ts')
-        .pipe(tsProject());
-
-    return merge([ // Merge the two output streams, so this task is finished when the IO of both operations are done.
-        tsResult.dts.pipe(gulp.dest('./release/definitions')),
-        tsResult.js.pipe(gulp.dest('./release/js'))
-    ]);
+    return gulp.src('./src/**/*.ts')
+        .pipe(tsProject())
+		.js.pipe(gulp.dest('./release/js'));
 });
 
 gulp.task('watch', gulp.series('scripts', function() {
-    gulp.watch('./src/**.ts', ['scripts']);
+    gulp.watch('./src/**/*.ts', ['scripts']);
 }));
 
 gulp.task('default', gulp.series('watch'));
